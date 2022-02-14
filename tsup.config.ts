b971c4b77cda43872074087f913449ccbb5af41c
Herbert <herbert.he0229@gmail.com>
2022-02-14 22:26:32 +0800

    chore :wrench: : update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "tsup"
 export const tsup = defineConfig({
     entry: ["src/index.ts"],
     clean: true,
-    format: ["esm"],
+    format: ["esm", "cjs"],
     dts: true,
     outDir: "dist",
+    minify: true
 })
