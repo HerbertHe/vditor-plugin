54c877d6195b510ec73181206d63131027f8fdf9
Herbert <herbert.he0229@gmail.com>
2022-02-14 22:33:12 +0800

    fix :bug: : fix bugs

--- src/helper.ts
+++ src/helper.ts
@@ -4,4 +4,7 @@ import { IVditorPlugin } from "./types"
  * Vditor Plugin Definition Helper
  * @param plugin
  */
-export const defineVditorPlugin = (plugin: IVditorPlugin) => plugin
+export const defineVditorPlugin = (plugin: IVditorPlugin) => {
+    plugin.compatible = plugin.compatible.replace(/[ |v]/g, "")
+    return
+}
